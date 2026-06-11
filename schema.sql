0b3a60d7a739dfd69c7d4131f3ec66f18f08d30c
Jhoao Pereyra <jhoao.pereyra@ucsp.edu.pe>
2026-06-11 02:47:15 -0500

    feat: tabla artilerias

--- schema.sql
+++ schema.sql
@@ -107,3 +107,9 @@ CREATE TABLE caballerias (
     tropa_id INT PRIMARY KEY REFERENCES tropas(tropa_id) ON DELETE CASCADE,
     bono_ataque_flanqueo NUMERIC(3,1) DEFAULT 0.0 NOT NULL CHECK (bono_ataque_flanqueo >= 0.0)
 );
+
+-- Tabla Hija: Artillería
+CREATE TABLE artillerias (
+    tropa_id INT PRIMARY KEY REFERENCES tropas(tropa_id) ON DELETE CASCADE,
+    bono_perforacion_plasma NUMERIC(3,1) DEFAULT 0.0 NOT NULL CHECK (bono_perforacion_plasma >= 0.0)
+);
